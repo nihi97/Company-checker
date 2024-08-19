56cc89e4a51dd6c053dd15ccfdd1f2b8ca1a0b35
nihi97 <137810040+nihi97@users.noreply.github.com>
2024-08-19 13:18:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Company-checker/",  // Base URL for GitHub Pages deployment
+  base: '/Company-checker/', // Set the correct base path for GitHub Pages
   build: {
-    outDir: 'dist',  // Output to a dist folder at the root level
-    rollupOptions: {
-      input: {
-        main: './index.html'  // Entry point is the root index.html
-      },
-    },
+    outDir: 'dist',
   },
 });
