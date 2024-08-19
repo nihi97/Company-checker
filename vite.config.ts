6e639bbf551b0c1293d3d35a39e08987abe3ce5f
nihi97 <137810040+nihi97@users.noreply.github.com>
2024-08-19 10:05:09 +0100

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/company_fit_evaluator/", // Adjust this if your repository name is different
+});
