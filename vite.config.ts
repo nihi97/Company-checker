fcd4a0483c17ea2d17ccf832cd1e455e2eac1b01
nihi97 <137810040+nihi97@users.noreply.github.com>
2024-08-19 12:13:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Company-checker/",  // Ensure this matches your GitHub Pages subdirectory
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
+  base: "/Company-checker/",  // Adjust this to match your GitHub Pages repository name
 });
